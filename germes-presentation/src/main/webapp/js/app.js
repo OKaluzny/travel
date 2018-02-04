228b8aaf4abfacc6fb94ba69435625f7658de61f
Andrii Holub <andr.holub@gmail.com>
2018-02-04 17:58:20 +0200

    improved l10n

--- germes-presentation/src/main/webapp/js/app.js
+++ germes-presentation/src/main/webapp/js/app.js
@@ -7,26 +7,10 @@ app.factory('cityService', [ '$resource', function($resource) {
 } ]);
 
 app.config(function($translateProvider) {
-	$translateProvider.translations('en', {
-		TITLE : 'Germes project',
-		CITIES : 'Cities',
-		CITY_NAME: 'Name',
-		CITY_DISTRICT : 'District',
-		CITY_REGION : 'Region'
+	$translateProvider.useStaticFilesLoader({
+	    prefix: 'l10n/locale-',
+	    suffix: '.json'
 	});
-	$translateProvider.translations('ru', {
-		TITLE : 'Проект Гермес',
-		CITIES : 'Населенные пункты',
-		CITY_NAME: 'Название',
-		CITY_DISTRICT : 'Район',
-		CITY_REGION : 'Область'
-	});
-	$translateProvider.translations('uk', {
-		TITLE : 'Проект Гермес',
-		CITIES : 'Населені пункти',
-		CITY_NAME: 'Назва',
-		CITY_DISTRICT : 'Район',
-		CITY_REGION : 'Область'
-	});	
 	$translateProvider.preferredLanguage('en');
+	$translateProvider.useSanitizeValueStrategy('escape');
 });
