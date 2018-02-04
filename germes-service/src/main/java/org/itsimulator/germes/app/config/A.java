ea27cc246e1cdea2f514fc4e7bd9f2078f0d90e1
Andrii Holub <andr.holub@gmail.com>
2018-02-04 15:59:55 +0200

    cleaning project

--- germes-service/src/main/java/org/itsimulator/germes/app/config/A.java
+++
@@ -1,8 +0,0 @@
-package org.itsimulator.germes.app.config;
-
-public class A {
-	public static void main(String[] args) throws ClassNotFoundException {
-		Class.forName("org.hibernate.dialect.SQLiteDialect");
-	}
-
-}
