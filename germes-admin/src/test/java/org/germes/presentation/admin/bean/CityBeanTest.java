6493a547d8a515413c4dd7a09a6cef1d8ffc60dc
Andrii Holub <andr.holub@gmail.com>
2018-02-06 22:09:28 +0200

    test cdi

---
+++ germes-admin/src/test/java/org/germes/presentation/admin/bean/CityBeanTest.java
@@ -0,0 +1,37 @@
+package org.germes.presentation.admin.bean;
+
+import static org.junit.Assert.*;
+
+import org.apache.commons.lang3.StringUtils;
+import org.itsimulator.germes.app.model.entity.geography.City;
+import org.junit.Test;;
+
+/**
+ * Verifies functionality of CityBean class
+ * @author Morenets
+ *
+ */
+public class CityBeanTest {
+	
+	@Test
+	public void clear_beanInitialized_allFieldsCleared() {
+		CityBean cityBean = new CityBean();
+		cityBean.setDistrict("test");
+		cityBean.setId(1);
+		cityBean.setName("test");
+		cityBean.setRegion("test");
+		cityBean.clear();
+		assertTrue(StringUtils.isEmpty(cityBean.getDistrict()));
+		assertTrue(StringUtils.isEmpty(cityBean.getName()));
+		assertTrue(StringUtils.isEmpty(cityBean.getRegion()));
+		assertTrue(cityBean.getId() == 0);
+	}
+	
+	@Test
+	public void untransform_cityInitialized_cityReturned() {
+		City city = new City();
+		CityBean cityBean = new CityBean();
+		City newCity = cityBean.untransform(city);
+		assertSame(city, newCity);
+	}	
+}
