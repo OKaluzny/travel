5e492d8cf702f5bca46185633661560692c4d3a0
Andrii Holub <andr.holub@gmail.com>
2018-02-15 20:54:22 +0200

    GlassFish 5

---
+++ germes-admin/restart-admin-jetty.bat
@@ -0,0 +1,5 @@
+docker stop admin
+docker rm admin
+
+docker build -t germes/admin -f src/main/resources/docker/admin-jetty.dockerfile .
+docker run -d --name=admin --link mysql:mysql -p 8080:8080 germes/admin
---
+++ germes-admin/src/main/resources/docker/admin-glassfish.dockerfile
@@ -0,0 +1,3 @@
+FROM glassfish
+
+ADD build/libs/admin.war /glassfish5/glassfish/domains/domain1/autodeploy/
