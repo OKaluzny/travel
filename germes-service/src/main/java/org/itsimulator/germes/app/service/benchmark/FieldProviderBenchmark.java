d69714080c6a327a3a1ee5aa2cc05477d4d72db6
Andrii Holub <andr.holub@gmail.com>
2018-02-17 19:43:59 +0200

    JMH configuration

--- germes-service/src/main/java/org/itsimulator/germes/app/service/benchmark/FieldProviderBenchmark.java
+++ germes-service/src/main/java/org/itsimulator/germes/app/service/benchmark/FieldProviderBenchmark.java
@@ -1,6 +1,5 @@
 package org.itsimulator.germes.app.service.benchmark;
 
-import java.util.Collection;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
@@ -9,7 +8,11 @@ import org.itsimulator.germes.app.model.entity.geography.City;
 import org.itsimulator.germes.app.model.entity.geography.Station;
 import org.itsimulator.germes.app.service.transform.impl.FieldProvider;
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.results.format.ResultFormatType;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.options.Options;
@@ -17,24 +20,29 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 /**
  * Class with JMH benchmarks for FieldProvider class
+ * 
  * @author Sergey
  *
  */
+@Warmup(iterations = 10)
+@Measurement(iterations = 10)
+@Fork(value = 2, jvmArgsAppend = "-server")
+@BenchmarkMode(Mode.AverageTime)
 public class FieldProviderBenchmark {
 
 	@Benchmark
-    public void testFieldProvider_getFieldNames() {
+	public void testFieldProvider_getFieldNames() {
 		FieldProvider provider = new FieldProvider();
 		provider.getFieldNames(City.class, CityCopy.class);
-    }
-	
+	}
+
 	public static void main(String[] args) throws Exception {
-		Options opts = new OptionsBuilder().include(".*").mode(Mode.AverageTime).timeUnit(TimeUnit.NANOSECONDS).jvmArgs("-server")
+		Options opts = new OptionsBuilder().include(".*").mode(Mode.AverageTime).timeUnit(TimeUnit.NANOSECONDS)
 				.resultFormat(ResultFormatType.TEXT).build();
 
 		new Runner(opts).run();
 	}
-	
+
 	public static class CityCopy extends AbstractEntity {
 		private String name;
 
