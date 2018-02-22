fcb52c689d48c03ed93f2ee4d35159d786d7af40
Andrii Holub <andr.holub@gmail.com>
2018-02-22 22:39:16 +0200

    angular components

--- germes-web/client/src/app/app.module.ts
+++ germes-web/client/src/app/app.module.ts
@@ -4,18 +4,34 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CitiesComponent } from './cities/cities.component';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService, TranslateLoader } from '@ngx-translate/core';
+import { LanguageComponent } from './language/language.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CitiesComponent
+    CitiesComponent,
+    LanguageComponent
   ],
   imports: [
     BrowserModule,
-    TranslateModule.forRoot()
+    HttpClientModule,
+    TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            }
+        })
   ],
-  providers: [],
+  providers: [TranslateService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http);
+}
